300f6f4917d23887e3c8a06438cabf7a8e687351
xiang3811 <271418976@qq.com>
2017-04-10 18:47:05 +0800

    20170410
    优化

--- audiomaketest1/audiomaketest1/audiomaketest1.cpp
+++ audiomaketest1/audiomaketest1/audiomaketest1.cpp
@@ -7,10 +7,7 @@
 #include <windows.h>  
 #pragma comment(lib, "winmm.lib")  
 
-void test()
-{
 
-}   
 /**/
 //RandomAccessFile fopen(String path) throws IOException {
 //	File f = new File(path);
@@ -71,36 +68,37 @@ void test()
 //	}
 //}
 //
-
-HWAVEIN hWaveIn;  //输入设备
-WAVEFORMATEX waveform; //采集音频的格式，结构体
-BYTE *pBuffer1;//采集音频时的数据缓存
-WAVEHDR wHdr1; //采集音频时包含数据缓存的结构体
-FILE *pf;
-int _tmain(int argc, _TCHAR* argv[])
+class CAudioRecording
 {
-	HANDLE          wait;
-	waveform.wFormatTag = WAVE_FORMAT_PCM;//声音格式为PCM
-	waveform.nSamplesPerSec = 8000;//采样率，16000次/秒
-	waveform.wBitsPerSample = 16;//采样比特，16bits/次
-	waveform.nChannels = 1;//采样声道数，2声道
-	waveform.nAvgBytesPerSec = 16000;//每秒的数据率，就是每秒能采集多少字节的数据
-	waveform.nBlockAlign = 2;//一个块的大小，采样bit的字节数乘以声道数
-	waveform.cbSize = 0;//一般为0
+private:
+	HWAVEIN hWaveIn;  //输入设备
+	WAVEFORMATEX waveform; //采集音频的格式，结构体
+	
+	WAVEHDR wHdr1; //采集音频时包含数据缓存的结构体
+public:
+	CAudioRecording() :hWaveIn(0) { Init(); }
+	~CAudioRecording() {
+		if(hWaveIn)
+			waveInClose(hWaveIn);
+	}
+	bool Init() {
+		HANDLE          wait;
+		waveform.wFormatTag = WAVE_FORMAT_PCM;//声音格式为PCM
+		waveform.nSamplesPerSec = 8000;//采样率，16000次/秒
+		waveform.wBitsPerSample = 16;//采样比特，16bits/次
+		waveform.nChannels = 1;//采样声道数，2声道
+		waveform.nAvgBytesPerSec = 16000;//每秒的数据率，就是每秒能采集多少字节的数据
+		waveform.nBlockAlign = 2;//一个块的大小，采样bit的字节数乘以声道数
+		waveform.cbSize = 0;//一般为0
 
-	wait = CreateEvent(NULL, 0, 0, NULL);
-	//使用waveInOpen函数开启音频采集
-	waveInOpen(&hWaveIn, WAVE_MAPPER, &waveform, (DWORD_PTR)wait, 0L, CALLBACK_EVENT);
-
-	//建立两个数组（这里可以建立多个数组）用来缓冲音频数据
-	DWORD bufsize = 1024 * 100;//每次开辟10k的缓存存储录音数据
-	int i = 20;
-	fopen_s(&pf, "录音测试.pcm", "wb");
-	while (i--)//录制20左右秒声音，结合音频解码和网络传输可以修改为实时录音播放的机制以实现对讲功能
-	{
-		pBuffer1 = new BYTE[bufsize];
-		wHdr1.lpData = (LPSTR)pBuffer1;
-		wHdr1.dwBufferLength = bufsize;
+		wait = CreateEvent(NULL, 0, 0, NULL);
+		//使用waveInOpen函数开启音频采集
+		MMRESULT error = waveInOpen(&hWaveIn, WAVE_MAPPER, &waveform, (DWORD_PTR)wait, 0L, CALLBACK_EVENT);
+		return error == 0;
+	}
+	int getWaveBuff(BYTE pBuff[], size_t size){
+		wHdr1.lpData = (LPSTR)pBuff;
+		wHdr1.dwBufferLength = size;
 		wHdr1.dwBytesRecorded = 0;
 		wHdr1.dwUser = 0;
 		wHdr1.dwFlags = 0;
@@ -110,12 +108,31 @@ int _tmain(int argc, _TCHAR* argv[])
 		waveInStart(hWaveIn);//开始录音
 		Sleep(1000);//等待声音录制1s
 		waveInReset(hWaveIn);//停止录音
-		fwrite(pBuffer1, 1, wHdr1.dwBytesRecorded, pf);
+
+		return wHdr1.dwBytesRecorded;
+	}
+
+};
+
+
+FILE *pf;
+int _tmain(int argc, _TCHAR* argv[])
+{
+	CAudioRecording audio;
+
+	BYTE *pBuffer1;//采集音频时的数据缓存
+	//建立两个数组（这里可以建立多个数组）用来缓冲音频数据
+	DWORD bufsize = 1024 * 100;//每次开辟10k的缓存存储录音数据
+	int i = 20;
+	fopen_s(&pf, "录音测试.pcm", "wb");
+	while (i--)//录制20左右秒声音，结合音频解码和网络传输可以修改为实时录音播放的机制以实现对讲功能
+	{
+		pBuffer1 = new BYTE[bufsize];
+		int lenth = audio.getWaveBuff(pBuffer1, bufsize);
+		fwrite(pBuffer1, 1, lenth, pf);
 		delete pBuffer1;
 		printf("%ds  ", i);
 	}
 	fclose(pf);
-
-	waveInClose(hWaveIn);
 	return 0;
 }
