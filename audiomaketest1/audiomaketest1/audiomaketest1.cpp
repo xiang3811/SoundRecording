665f4fef36a6f41cd8c0501d620973289e2a87cf
xiang3811 <271418976@qq.com>
2019-08-01 17:13:12 +0800

    out wav head test

--- audiomaketest1/audiomaketest1/audiomaketest1.cpp
+++ audiomaketest1/audiomaketest1/audiomaketest1.cpp
@@ -68,6 +68,27 @@
 //	}
 //}
 //
+typedef struct _wave_pcm_hdr
+{
+	char riff[4]; // = "RIFF"
+	int size_8; // = FileSize - 8
+	char wave[4]; // = "WAVE"
+	char fmt[4]; // = "fmt "
+	int fmt_size;// = 下一个结构体的大小 : 16
+
+	short int format_tag; // = PCM : 1
+	short int channels; // = 通道数 : 1
+	int samples_per_sec; // = 采样率 : 8000 | 6000 | 11025 | 16000
+	int avg_bytes_per_sec; // = 每秒字节数 : samples_per_sec * bits_per_sample / 8
+	short int block_align; // = 每采样点字节数 : wBitsPerSample / 8
+	short int bits_per_sample; // = 量化比特数: 8 | 16
+
+	char data[4]; // = "data";
+	int data_size; // = 纯数据长度 : FileSize - 44 
+} wave_pcm_hdr;
+
+
+
 class CAudioRecording
 {
 private:
@@ -118,6 +139,24 @@ public:
 FILE *pf;
 int _tmain(int argc, _TCHAR* argv[])
 {
+// 	/* 默认wav音频头部数据 */
+// 	wave_pcm_hdr default_wav_hdr =
+// 	{
+// 		{ 'R', 'I', 'F', 'F' },
+// 		0,
+// 		{ 'W', 'A', 'V', 'E' },
+// 		{ 'f', 'm', 't', ' ' },
+// 		16,
+// 		1,
+// 		1,
+// 		8000,
+// 		16000,
+// 		2,
+// 		16,
+// 		{ 'd', 'a', 't', 'a' },
+// 		0
+// 
+// 	};
 	CAudioRecording audio;
 
 	BYTE *pBuffer1;//采集音频时的数据缓存
@@ -129,6 +168,8 @@ int _tmain(int argc, _TCHAR* argv[])
 	{
 		pBuffer1 = new BYTE[bufsize];
 		int lenth = audio.getWaveBuff(pBuffer1, bufsize);
+// 		wave_pcm_hdr wav_hdr = default_wav_hdr;
+// 		fwrite(&wav_hdr, sizeof(wav_hdr), 1, pf);//头
 		fwrite(pBuffer1, 1, lenth, pf);
 		delete pBuffer1;
 		printf("%ds  ", i);
